4132546e0065abce244bdd82a537325506309f65
Kotapka <bartek.kotapka32@gmail.com>
2022-04-29 12:43:17 +0200

    Lab8

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Test1 from "./components/1-componentClass";
+import Test2 from "./components/2-componentFunctional";
+import QuizComponent from "./components/quiz-component";
+
+function App() {
+  return (
+    <div className="App">
+      <QuizComponent/>
+    </div>
+  );
+}
+
+export default App;
